5b37f5d390a9f8a2a3b9818c70bf6e308f2317ef
jitendra kumar <githubartist@gmail.com>
2024-12-18 19:52:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// =========== Start of Navbar ===========
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const navbar = document.getElementById("navbar");
+
+if (bar) {
+    bar.addEventListener("click", ()=> {
+        navbar.classList.add("active");
+
+    })
+}
+
+if (close) {
+    close.addEventListener("click", ()=> {
+        navbar.classList.remove("active")
+    })
+}
+
+
+// =========== End of Navbar ===========
